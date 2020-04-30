022d8346a581ccbbb879b602b5c64836abd0a343
Klachkov Valery <0gdump.kv@gmail.com>
2020-04-30 22:51:26 +1000

    Implement demo activity

--- app/src/main/java/open/geosolve/canvasdemo/ui/MainActivity.kt
+++ app/src/main/java/open/geosolve/canvasdemo/ui/MainActivity.kt
@@ -1,27 +1,60 @@
 package open.geosolve.canvasdemo.ui
 
 import android.os.Bundle
+import android.widget.SeekBar
+import kotlinx.android.synthetic.main.activity_main.*
 import moxy.MvpAppCompatActivity
 import moxy.ktx.moxyPresenter
+import open.geosolve.canvasdemo.R
 import open.geosolve.canvasdemo.model.Point
 import open.geosolve.canvasdemo.presentation.MainPresenter
 import open.geosolve.canvasdemo.presentation.MainView
-import open.geosolve.canvasdemo.ui.view.EditableCanvasView
 
 class MainActivity : MvpAppCompatActivity(), MainView {
 
     private val presenter by moxyPresenter { MainPresenter() }
-    private lateinit var content: EditableCanvasView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
 
-        content = EditableCanvasView(this)
+        updateScaleFromSeekbar(0f)
 
-        setContentView(content)
+        scale.setOnSeekBarChangeListener(object : SeekBar.OnSeekBarChangeListener {
+
+            override fun onProgressChanged(seekBar: SeekBar?, progress: Int, fromUser: Boolean) {
+                updateScaleFromSeekbar(progress.toFloat())
+            }
+
+            override fun onStartTrackingTouch(seekBar: SeekBar?) {}
+
+            override fun onStopTrackingTouch(seekBar: SeekBar?) {}
+        })
+    }
+
+    private fun updateScaleFromSeekbar(progress: Float) {
+        canvas.updateScale(
+            mapValueToRange(
+                progress,
+                0f,
+                100f,
+                0.2f,
+                10f
+            )
+        )
+    }
+
+    private fun mapValueToRange(
+        x: Float,
+        inMin: Float,
+        inMax: Float,
+        outMin: Float,
+        outMax: Float
+    ): Float {
+        return (x - inMin) * (outMax - outMin) / (inMax - inMin) + outMin
     }
 
     override fun attachPoint(point: Point) {
-        content.attachPoint(point)
+        canvas.attachPoint(point)
     }
 }
